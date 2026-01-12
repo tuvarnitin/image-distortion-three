b254da90674f00aa0459f163dfa917e28bb1dd5a
Nitin <thissidenitin@gmail.com>
2026-01-12 17:03:56 +0530

    changed images paths thats are causing on production

--- src/images.js
+++ src/images.js
@@ -1,51 +1,51 @@
 export const images = [
     {
         name: "Giyu Tomioka",
-        path: "/public/images/GiyuTomioka.jpg",
+        path: "/images/GiyuTomioka.jpg",
     },
     {
         name: "Gyomei Himejima",
-        path: "/public/images/GyomeiHimejima.jpg",
+        path: "/images/GyomeiHimejima.jpg",
     },
     {
         name: "Inosuke Hashibira",
-        path: "/public/images/InosukeHashibira.jpg",
+        path: "/images/InosukeHashibira.jpg",
     },
     {
         name: "Kanae Kocho",
-        path: "/public/images/KanaeKocho.jpg",
+        path: "/images/KanaeKocho.jpg",
     },
     {
         name: "Mitsuri Kanroji",
-        path: "/public/images/MitsuriKanroji.jpg",
+        path: "/images/MitsuriKanroji.jpg",
     },
     {
         name: "Muichiro Tokito",
-        path: "/public/images/MuichiroTokito.jpg",
+        path: "/images/MuichiroTokito.jpg",
     },
     {
         name: "Nezuko Kamado",
-        path: "/public/images/NezukoKamado.jpg",
+        path: "/images/NezukoKamado.jpg",
     },
     {
         name: "Obanai Iguro",
-        path: "/public/images/ObanaiIguro.jpg",
+        path: "/images/ObanaiIguro.jpg",
     },
     {
         name: "Shinazugawa",
-        path: "/public/images/Shinazugawa.jpg",
+        path: "/images/Shinazugawa.jpg",
     },
     {
         name: "Shinjuro Rengoku",
-        path: "/public/images/ShinjuroRengoku.jpg",
+        path: "/images/ShinjuroRengoku.jpg",
     },
     {
         name: "Shinobu Kocho",
-        path: "/public/images/ShinobuKocho.jpg",
+        path: "/images/ShinobuKocho.jpg",
     },
     {
         name: "Zenitsu Agatsuma",
-        path: "/public/images/ZenitsuAgatsuma.jpg",
+        path: "/images/ZenitsuAgatsuma.jpg",
     },
     
     
